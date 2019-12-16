b57f7f12388b591a53e308980f14476752c988a8
meghna-dash <meghna@auburn.edu>
2019-12-16 17:51:29 -0500

    initial landing code

---
+++ landing/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Main from './pages/Main';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
