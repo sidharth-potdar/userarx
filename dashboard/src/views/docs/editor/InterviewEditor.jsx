a14a661824be9553174dc78416c1ab7b8996f4e0
meghna-dash <meghna@auburn.edu>
2019-12-23 11:47:43 -0500

    adding text and entity states

--- dashboard/src/views/docs/editor/InterviewEditor.jsx
+++ dashboard/src/views/docs/editor/InterviewEditor.jsx
@@ -14,13 +14,68 @@ const inlineToolbarPlugin = createInlineToolbarPlugin({
 });
 const { InlineToolbar } = inlineToolbarPlugin;
 const plugins = [inlineToolbarPlugin];
-const text = 'The Kick Six (also known as Kick Bama Kick) was the final play of the 78th Iron Bowl college football game played on November 30, 2013 at Jordan–Hare Stadium in Auburn, Alabama.';
+const text = "Book 2 tickets from Seattle to Cairo";
+const entities: IEntity[] = [
+  {
+    id: '1',
+    name: 'number',
+    type: 'number'
+  },
+  {
+    id: '2',
+    name: 'sourceCity',
+    type: 'string'
+  },
+  {
+    id: '3',
+    name: 'destinationCity',
+    type: 'string'
+  }
+]
+
+const labeledEntities: models.ILabel<models.IEntityData<IEntity>>[] = [
+  {
+    startIndex: 5,
+    endIndex: 6,
+    data: {
+      option: { ...entities[0] },
+      text: "2",
+      displayName: entities[0].name,
+      original: entities[0]
+    }
+  },
+  {
+    startIndex: 20,
+    endIndex: 27,
+    data: {
+      option: { ...entities[1] },
+      text: "Seattle",
+      displayName: entities[1].name,
+      original: entities[1]
+    }
+  },
+  {
+    startIndex: 31,
+    endIndex: 36,
+    data: {
+      option: { ...entities[2] },
+      text: "Cairo",
+      displayName: entities[2].name,
+      original: entities[2]
+    }
+  }
+]
+
 
 class InterviewEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
       editorState: createEditorStateWithText(text),
+      entities: [...entities],
+      labeledEntities: [...labeledEntities],
+      isNewEntityVisible: false,
+      newEntityName: ''
     };
     this.onChange = this.onChange.bind(this);
     this.focus = this.focus.bind(this);
@@ -35,6 +90,47 @@ class InterviewEditor extends Component {
   focus = () => {
     this.editor.focus();
   };
+
+  onChangeLabeledEntities = (labeledEntities: models.ILabel<any>[]) => {
+    this.setState({
+      labeledEntities
+    })
+  }
+
+  onClickReset = () => {
+    this.setState({
+      entities,
+      labeledEntities,
+      readOnly: false
+    })
+  }
+
+  onClickNewEntity = () => {
+    console.log(`onClickNewEntity`)
+    this.setState({
+      isNewEntityVisible: true
+    })
+  }
+
+  onClickSubmitNewEntity = () => {
+    const newEntity: IEntity = {
+      id: new Date().toJSON(),
+      name: this.state.newEntityName,
+      type: 'string'
+    }
+
+    this.setState(prevState => ({
+      entities: [...prevState.entities, newEntity],
+      newEntityName: '',
+      isNewEntityVisible: false
+    }))
+  }
+
+  onChangeEntityName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      newEntityName: event.target.value
+    })
+  }
 
   componentDidMount() {
     this.focus();
