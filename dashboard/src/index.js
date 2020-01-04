cbfedd67846541e02e9a29cf8e37db2399e7034e
meghna-dash <meghna@auburn.edu>
2020-01-03 21:02:24 -0500

    routing

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
-import AuthLayout from "layouts/Auth/Auth.jsx";
 import AdminLayout from "layouts/Admin/Admin.jsx";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -15,7 +14,6 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/auth" render={props => <AuthLayout {...props} />} />
       <Route path="/admin" render={props => <AdminLayout {...props} />} />
       <Redirect from="/" to="/admin/dashboard" />
     </Switch>
