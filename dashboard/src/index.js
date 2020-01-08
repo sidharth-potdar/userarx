2c8b27883e7fc510a720f8082124ae8175de5546
meghna-dash <meghna@auburn.edu>
2020-01-08 12:43:28 -0600

    index.js vs app.js

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,22 +1,45 @@
 import React from "react";
+import App from './App.js';
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-
 import AdminLayout from "layouts/Admin/Admin.jsx";
+import Amplify from 'aws-amplify';
+//
+// import AWSAppSyncClient from "aws-appsync";
+// import { Rehydrated } from 'aws-appsync-react';
+// import { ApolloProvider } from 'react-apollo';
+
+import awsconfig from './aws-exports'
+import appSyncConfig from './appsync.js';
 
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss";
 import "assets/demo/demo.css";
 
+
+Amplify.configure(awsconfig)
+
 const hist = createBrowserHistory();
 
+// const client = new AWSAppSyncClient({
+//   url: appSyncConfig.aws_appsync_graphqlEndpoint,
+//   region: appSyncConfig.aws_appsync_region,
+//   auth: {
+//     type: appSyncConfig.aws_appsync_authenticationType,
+//     apiKey: appSyncConfig.aws_appsync_apiKey,
+//   }
+// });
+//
+// const WithProvider = () => (
+//   <ApolloProvider client={client}>
+//     <Rehydrated>
+//       <App/>
+//     </Rehydrated>
+//   </ApolloProvider>
+// );
+
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/" render={props => <AdminLayout {...props} />} />
-      <Redirect from="/" to="/dashboard" />
-    </Switch>
-  </Router>,
+  <App/>,
   document.getElementById("root")
 );
